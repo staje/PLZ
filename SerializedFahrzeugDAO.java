8825fffb22287f073e5087fb6d9916d167b365bf
staje <33398035+staje@users.noreply.github.com>
2017-11-06 20:43:27 +0100

    Create SerializedFahrzeugDAO.java

---
+++ SerializedFahrzeugDAO.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+/**
+ * @author <Nemanja filipovic>
+ * Matrikelnummer:01348905
+ */
+
+public class SerializedFahrzeugDAO implements FahrzeugDAO {
+
+	@Override
+	public List<Fahrzeug> getFahrzeugList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Fahrzeug getFahrzeugbyId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void speichereFahrzeug(Fahrzeug fahrzeug) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loescheFahrzeug(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
